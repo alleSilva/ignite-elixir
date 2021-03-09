ac2cd5efb12344b854bcf0095f0b6d1037fdc107
Alesandro Silva <silva_alesandro@outlook.com>
2021-03-08 23:26:22 -0400

    feat: return the length of a list using recursion

--- chapter-1/challenges/list_length/lib/list_length.ex
+++ chapter-1/challenges/list_length/lib/list_length.ex
@@ -1,18 +1,10 @@
 defmodule ListLength do
-  @moduledoc """
-  Documentation for `ListLength`.
-  """
+  def call(list), do: length(list, 0)
 
-  @doc """
-  Hello world.
+  defp length([], acc), do: acc
 
-  ## Examples
-
-      iex> ListLength.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  defp length([head | tail], acc) do
+    acc = acc + 1
+    length(tail, acc)
   end
 end
