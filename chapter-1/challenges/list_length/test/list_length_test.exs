e41f6720e3ff5917c5172a8352e79e9f7eaf7d26
Alesandro Silva <silva_alesandro@outlook.com>
2021-03-09 06:35:55 -0400

    test: verify the return of the function ListLength.call/1 that return the length of a list

--- chapter-1/challenges/list_length/test/list_length_test.exs
+++ chapter-1/challenges/list_length/test/list_length_test.exs
@@ -1,8 +1,16 @@
 defmodule ListLengthTest do
   use ExUnit.Case
-  doctest ListLength
 
-  test "greets the world" do
-    assert ListLength.hello() == :world
+  describe "call/1" do
+    test "returns the list length" do
+
+      list = [1, 2, 3, 4, 5]
+
+      response = ListLength.call(list)
+
+      expected_reaponse = 5
+
+      assert response == expected_reaponse
+    end
   end
 end
